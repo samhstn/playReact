d19ef3bd5ddb1032ea0c4d488e18f5fa6e10475e
shouston3 <shouston3@me.com>
2016-03-14 21:36:04 +0000

    basic setup

---
+++ app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Components from './components.js';
+
+const rootElement = document.getElementById('container');
+
+ReactDOM.render(
+    <Components />,
+    rootElement
+)
