813b72779623e8d59d480ed0303d0eb3f363fd2d
shouston3 <shouston3@me.com>
2016-03-14 18:44:54 +0000

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+M
+rts = {
+  entry: {
+    javascript: './app.js',
+    html: './index.html'
+  },
+  output: {
+    filename: 'app.js',
+    path: __dirname + '/dist'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ['react-hot', 'babel']
+      },
+      {
+        test: /\.html$/,
+        loader: 'file?name=[name].[ext]'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      }
+    ]
+  }
+};
+
